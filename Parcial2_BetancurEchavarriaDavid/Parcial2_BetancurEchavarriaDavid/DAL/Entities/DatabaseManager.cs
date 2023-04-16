33c110957f5907df13f22bbe33e0770e87f42597
DAVID BETANCUR ECHAVARRIA <davidbetancur262776@correo.itm.edu.co>
2023-04-16 10:01:19 -0500

    Creation table Tickets

--- Parcial2_BetancurEchavarriaDavid/Parcial2_BetancurEchavarriaDavid/DAL/Entities/DatabaseManager.cs
+++ Parcial2_BetancurEchavarriaDavid/Parcial2_BetancurEchavarriaDavid/DAL/Entities/DatabaseManager.cs
@@ -1,6 +1,29 @@
-﻿namespace Parcial2_BetancurEchavarriaDavid.DAL.Entities
+﻿using System.Data.SqlClient;
+
+namespace Parcial2_BetancurEchavarriaDavid.DAL.Entities
 {
-    public class DatabaseManager
-    {
-    }
+    
+        public class DatabaseManager
+        {
+            private const string connectionString = "Data Source=(local);Initial Catalog=master;Integrated Security=True";
+
+            public void CreateDatabase()
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Create database
+                    SqlCommand cmd = new SqlCommand("CREATE DATABASE ConcertDB", connection);
+                    cmd.ExecuteNonQuery();
+
+                    // Create table
+                    cmd = new SqlCommand("USE ConcertDB; CREATE TABLE Tickets (TicketID int PRIMARY KEY, UseDate datetime NULL, IsUsed bit DEFAULT 0, EntranceGate varchar(50) NULL)", connection);
+                    cmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
+        }
+    
 }
